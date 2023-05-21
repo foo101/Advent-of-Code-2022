15b4c389486f9e847472e94dbfad628002178287
foo101 <hielkek@hotmail.com>
2023-05-21 09:08:21 +0200

    day 1
    advent of code 2022 day 1

--- day 1/code.py
+++ day 1/code.py
@@ -1,13 +1,30 @@
-import sys
 import os
-with open(os.path.join(sys.path[0], "data.txt"), "r") as f:
-    max = 0
-    cur = 0
-    while(f.length() > 0):
-        line = f.readline()
-        if(line):
-            cur += line
+
+os.chdir("day 1")
+
+calories_per_elf = [] # list of elves and their calories
+sum_of_calories = 0 # calory sum tracker
+
+for str_in in open("data.txt", "rt"):
+    # val = 0 # non-pythonic, can be ignored
+    try:
+        if(str_in[:-1]): # check if line is empty
+            calories = int(str_in[:-1]) # take input, strip \n and convert to int
+            # print(val)
+            sum_of_calories += calories
         else:
-            if(max < cur):
-                max = cur
-                cur = 0
+            calories_per_elf.append(sum_of_calories)
+            sum_of_calories = 0
+    except ValueError:
+        print("an issue occured, go fix")
+
+calories_per_elf.sort(reverse=True) # sort elves in descending order
+print("encountered calories per elf:", calories_per_elf)
+
+print("highest calory amount:", calories_per_elf[0])
+
+top_calories = 0
+for elf in range(3):
+    top_calories += calories_per_elf[elf]
+
+print("sum of top 3 elves:", top_calories)
