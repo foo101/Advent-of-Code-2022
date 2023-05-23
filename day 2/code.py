1e9c7863a580c2eef94aaf09bca8fdd997080225
foo101 <hielkek@hotmail.com>
2023-05-23 20:20:14 +0200

    day 2 part 1 and 2 with dicts
    updated day 2 part 1 and 2 to use dicts as lookup tables, making it a whole lot less complex

--- day 2/code.py
+++ day 2/code.py
@@ -2,89 +2,61 @@ import os
 
 os.chdir("day 2")
 
+
+
 rps_results = []
-rps_sum_of_results = {"wins":0, "draws":0, "losses":0}
-
-rps_table_of_score_bonus_per_outcome = {"wins":6, "draws":3, "losses":0}
-rps_table_of_score_bonus_per_play = {"X":1, "Y":2, "Z":3}
-
-rps_score = 0 # score tracker
-
 for str_in in open("data.txt", "rt"):
     result = str_in[:-1].split(sep=' ')
     # print(result) # result is a list with opponent's play and own play
     rps_results.append(result)
 
-rps_score_bonus_per_play = {"X":1, "Y":2, "Z":3}
+rps_table_of_score_bonus_per_outcome = {"wins":6, "draws":3, "losses":0}
+rps_table_of_score_bonus_per_play = {"X":1, "Y":2, "Z":3}
 
-for result in rps_results:
-    rps_suggested_play = result[1]
-    outcome = (ord(result[1]) - ord(result[0]) - 23) % 3 # 0 if draw, 1 if win, 2 if loss
-    # per outcome, calculate score for win/loss/draw
-    if(outcome == 1):
-        rps_sum_of_results["wins"] += 1
-        rps_score += rps_table_of_score_bonus_per_outcome["wins"]
-    elif(outcome == 2):
-        rps_sum_of_results["losses"] += 1
-        rps_score += rps_table_of_score_bonus_per_outcome["losses"]
-    elif(outcome == 0):
-        rps_sum_of_results["draws"] += 1
-        rps_score += rps_table_of_score_bonus_per_outcome["draws"]
-    else:
-        print("Something went wrong at parsing the outcome of", result)
+# part 1: play whatever the guide tells you
 
-    # per outcome, calculate score for play
-    if(result[1] in rps_score_bonus_per_play.keys()):
-        rps_score += rps_score_bonus_per_play[rps_suggested_play]
-    else:
-        print("something went wrong at parsing the result of", str_in)
-
-# scores if you follow the plan
-print("score after the match (complex plan):", rps_score)
-
-
-# scores with X = loss, Y = draw, Z = win
-rps_suggested_plays = {
-    "A":{"X":"Z", "Y":"X", "Z":"Y"}, 
-    "B":{"X":"X", "Y":"Y", "Z":"Z"}, 
-    "C":{"X":"Y", "Y":"Z", "Z":"X"}}
-
-rps_score = 0
-rps_sum_of_results = {"wins":0, "draws":0, "losses":0}
-rps_sum_of_plays = {"X":0, "Y":0, "Z":0}
-
-for result in rps_results:
-    rps_suggested_play = rps_suggested_plays[result[0]][result[1]]
-    rps_score_this_match = 0
-    if(result[1] == 'X'):
-        rps_sum_of_results["wins"] += 1
-        rps_score_this_match += rps_table_of_score_bonus_per_outcome["wins"]
-    elif(result[1] == 'Z'):
-        rps_sum_of_results["losses"] += 1
-        rps_score_this_match += rps_table_of_score_bonus_per_outcome["losses"]
-    elif(result[1] == 'Y'):
-        rps_sum_of_results["draws"] += 1
-        rps_score_this_match += rps_table_of_score_bonus_per_outcome["draws"]
-    else:
-        print("something went wrong at parsing the result of", result)
+def calc_score_by_play(plays: list[list[str]]) -> int:
+    rps_outcome_per_play = {
+        "A":{"X":"draws", "Y":"wins", "Z":"losses"}, 
+        "B":{"X":"losses", "Y":"draws", "Z":"wins"}, 
+        "C":{"X":"wins", "Y":"losses", "Z":"draws"}}
     
-    rps_score_this_match += rps_score_bonus_per_play[rps_suggested_play]
-    rps_sum_of_plays[rps_suggested_play] += 1
-    # print("result:", result, "play:", rps_suggested_play, "score:", rps_score_this_match)
-    rps_score += rps_score_this_match
+    score = 0
+    for play in plays:
+        their_play = play[0]
+        my_play = play[1]
+        outcome = rps_outcome_per_play[their_play][my_play]
 
-# print(rps_sum_of_results)
-# print(rps_sum_of_plays)
+        score += rps_table_of_score_bonus_per_play[my_play]
+        score += rps_table_of_score_bonus_per_outcome[outcome]
 
-print("score after the match (complex plan, play determines outcome):", rps_score)
+        # print(play, outcome, rps_table_of_score_bonus_per_play[my_play], rps_table_of_score_bonus_per_outcome[outcome])
+    return score
 
-# print("amount of results:", len(rps_results))
+print(calc_score_by_play(rps_results)) # TODO: apply to full dataset
 
-rps_score = 0
-for res in rps_sum_of_results.keys():
-    rps_score += rps_table_of_score_bonus_per_outcome[res] * rps_sum_of_results[res]
+#part 2: play whatever the guide tells you is the outcome
+def calc_score_by_outcome(plays: list[list[str]]) -> int:
+    rps_translate_outcome = {
+        "X":"losses", 
+        "Y":"draws", 
+        "Z":"wins"}
+    
+    rps_play_per_outcome = {
+        "A":{"wins":"Y", "draws":"X", "losses":"Z"}, 
+        "B":{"wins":"Z", "draws":"Y", "losses":"X"}, 
+        "C":{"wins":"X", "draws":"Z", "losses":"Y"}}
+    
+    score = 0
+    for play in plays:
+        their_play = play[0]
+        outcome = rps_translate_outcome[play[1]]
+        my_play = rps_play_per_outcome[their_play][outcome]
 
-for res in rps_sum_of_plays.keys():
-    rps_score += rps_score_bonus_per_play[res] * rps_sum_of_plays[res]
+        score += rps_table_of_score_bonus_per_play[my_play]
+        score += rps_table_of_score_bonus_per_outcome[outcome]
 
-print("score after the match (complex plan, play determines outcome):", rps_score)
+        # print(play, outcome, rps_table_of_score_bonus_per_play[my_play], rps_table_of_score_bonus_per_outcome[outcome])
+    return score
+
+print(calc_score_by_outcome(rps_results)) # TODO: apply to full dataset
